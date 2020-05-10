bd07bb66a7460011383aadd2229fe1c0d874d939
leeyunhome <leeyunhome@daum.net>
2020-05-10 18:04:58 +0900

    count number of e in the string, print

---
+++ python_cheetsheet_test1.py
@@ -0,0 +1,14 @@
+# x = 1
+# y = 2
+# print("v=", 3, "cm :", x, ",", y+4)
+
+# s = "leehyerim"
+# cnt = 0
+
+# for c in s:
+#     if c == 'e':
+#         cnt = cnt + 1
+# print("found", cnt, "'e'")
+
+f = open("file.txt", "w", encoding="utf8")
+f.read([n])
